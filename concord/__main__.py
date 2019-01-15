5f13d78bdb580276ded5dce9828663e9ac705f2d
Nathan Klapstein <nklapste@ualberta.ca>
2019-01-15 09:53:31 -0700

    adding skeleton to concord

--- concord/__main__.py
+++ concord/__main__.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+"""main entrypoint for concord"""
+
+import sys
+
+
+def main() -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
