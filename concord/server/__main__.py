19f329e0ad9d801e4b5324873c845ef3bd1547da
Nathan Klapstein <nklapste@ualberta.ca>
2019-01-16 15:37:58 -0700

    adding basic argparse stub

--- concord/server/__main__.py
+++ concord/server/__main__.py
@@ -3,10 +3,37 @@
 
 """main entrypoint for the concord server"""
 
+import argparse
 import sys
+from logging import getLogger
+
+from concord.common import add_log_parser
+
+__log__ = getLogger(__name__)
 
 
-def main() -> int:
+def get_parser() -> argparse.ArgumentParser:
+    """Create and return the argparser for concord flask/cheroot server"""
+    parser = argparse.ArgumentParser(
+        description="Start the concord flask/cheroot server",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    group = parser.add_argument_group("server")
+    group.add_argument("-d", "--host", default='0.0.0.0',
+                       help="Hostname to listen on")
+    group.add_argument("-p", "--port", default=8080, type=int,
+                       help="Port of the webserver")
+    group.add_argument("--debug", action="store_true",
+                       help="Run the server in Flask debug mode")
+    add_log_parser(parser)
+
+    return parser
+
+
+def main(argv=sys.argv[1:]) -> int:
+    parser = get_parser()
+    args = parser.parse_args(argv)
     return 0
 
 
