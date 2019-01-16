2f91b6ff1ddedd6f7bfac2ade7059fbf23f803e0
Nathan Klapstein <nklapste@ualberta.ca>
2019-01-16 15:55:29 -0700

    adding stub argparser

--- concord/scraper/__main__.py
+++ concord/scraper/__main__.py
@@ -3,10 +3,27 @@
 
 """main entrypoint for the concord Discord scraper"""
 
+import argparse
 import sys
 
+from concord.common import add_log_parser
 
-def main() -> int:
+
+def get_parser() -> argparse.ArgumentParser:
+    """Create and return the argparser for concord Discord scraper"""
+    parser = argparse.ArgumentParser(
+        description="Start the concord Discord scraper",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    add_log_parser(parser)
+
+    return parser
+
+
+def main(argv=sys.argv[1:]) -> int:
+    parser = get_parser()
+    args = parser.parse_args(argv)
     return 0
 
 
